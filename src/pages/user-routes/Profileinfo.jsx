40bc10f1c79130e8bb4446c944413345fddf4e24
USER <USER@DESKTOP-GAP4S8J>
2024-11-27 18:11:08 +0530

    ddfdfdfdf

--- src/pages/user-routes/Profileinfo.jsx
+++ src/pages/user-routes/Profileinfo.jsx
@@ -1,22 +1,32 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Base from '../../components/Base'
 import { Card, CardBody, CardText, Col, Container, Row } from 'reactstrap';
-import { Link, useParams } from 'react-router-dom'
+import { json, Link, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
 import userContext from '../../context/userContext';
 import { loadUserDetailsById } from '../../services/user-service';
 function Profileinfo() {
   const {Id}=useParams();
 // const object=useContext(userContext);
-   const[user,setUser]=useState({});
-   useEffect(()=>{
-   loadUserDetailsById(Id).then(data=>{
-  console.log(data);
-  setUser(...data);
-}).catch(error=>{
-  console.log(error);
-})
-  },[])
+   const[user,setUser]=useState(null);
+
+
+    // Loading Details...
+  useEffect(() => {
+    loadUserDetailsById(Id)
+      .then((data) => {
+        console.log('Datas:', data);
+        setUser(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error in Loading Comments');
+      });
+  }, []);
+
+
+
   return (
     <Base>
       <Container className='mt-5 mb-5'>
@@ -32,6 +42,7 @@ function Profileinfo() {
       <CardBody>
  <CardText>
 <h6  style={{color:"red"}} >
+
           Welcome user:{user && user.name && user.name }
           </h6>
           <h6  style={{color:"blue"}} >
